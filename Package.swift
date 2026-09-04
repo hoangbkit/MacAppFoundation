11b031d42efab9ee59b12b62264b59f6ba4a3095
Hoang Nguyen <hoangbkit@gmail.com>
2026-09-04 07:14:33 +0700

    feat: support macOS 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "MacAppFoundation",
     platforms: [
-        .macOS("26.0")
+        .macOS("15.0")
     ],
     products: [
         .library(
