d16540598d53f7189b4b860b7677b6118a0b1746
Hoang Nguyen <hoangbkit@gmail.com>
2026-09-04 08:09:21 +0700

    refactor: remove dead purchase mode helper

--- Sources/MacAppFoundation/Commerce/PurchaseServiceFactory.swift
+++ Sources/MacAppFoundation/Commerce/PurchaseServiceFactory.swift
@@ -8,16 +8,6 @@ enum PurchaseServiceMode: Sendable, Equatable {
 
 /// Creates the package's live StoreKit service or, in Debug builds, its in-process simulator.
 enum PurchaseServiceFactory {
-    /// Returns the mode that can actually run in the current build.
-    /// Release builds always resolve to live StoreKit.
-    static func effectiveMode(for requestedMode: PurchaseServiceMode) -> PurchaseServiceMode {
-        #if DEBUG
-        requestedMode
-        #else
-        .live
-        #endif
-    }
-
     @MainActor
     static func make(
         mode requestedMode: PurchaseServiceMode = .live,
