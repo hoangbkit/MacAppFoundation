f3fb503dfdc308cc1e49b0d098711f9036c5ca75
Hoang Nguyen <hoangbkit@gmail.com>
2026-09-08 07:50:05 +0700

    Add convenient built-in theme accessors

---
+++ Sources/MacAppFoundation/Theme/MacAppTheme+BuiltIns.swift
@@ -0,0 +1,14 @@
+public extension MacAppTheme {
+    static var githubDarkDimmed: MacAppTheme { MacAppThemeCatalog.githubDarkDimmed }
+    static var midnight: MacAppTheme { MacAppThemeCatalog.midnight }
+    static var ocean: MacAppTheme { MacAppThemeCatalog.ocean }
+    static var aurora: MacAppTheme { MacAppThemeCatalog.aurora }
+    static var ember: MacAppTheme { MacAppThemeCatalog.ember }
+    static var graphite: MacAppTheme { MacAppThemeCatalog.graphite }
+    static var porcelain: MacAppTheme { MacAppThemeCatalog.porcelain }
+    static var blossom: MacAppTheme { MacAppThemeCatalog.blossom }
+    static var morningMist: MacAppTheme { MacAppThemeCatalog.morningMist }
+    static var softSage: MacAppTheme { MacAppThemeCatalog.softSage }
+    static var sunrise: MacAppTheme { MacAppThemeCatalog.sunrise }
+    static var githubLight: MacAppTheme { MacAppThemeCatalog.githubLight }
+}
