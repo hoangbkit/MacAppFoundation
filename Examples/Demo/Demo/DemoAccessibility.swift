9cd1d8d615e480f0d6592e6b3b982fa70f6f1e5d
Hoang Nguyen <hoangbkit@gmail.com>
2026-09-08 19:49:16 +0700

    test: add stable demo accessibility identifiers

---
+++ Examples/Demo/Demo/DemoAccessibility.swift
@@ -0,0 +1,6 @@
+enum DemoAccessibilityID {
+    static let onboarding = "maf.demo.onboarding"
+    static let onboardingBack = "maf.demo.onboarding.back"
+    static let onboardingContinue = "maf.demo.onboarding.continue"
+    static let settings = "maf.demo.settings"
+}
